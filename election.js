25f960b37b8ee7698bbf13b6d156d09cd029ec9c
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-11 15:17:21 -0400

    Finished Part 1

--- election.js
+++ election.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    data: {},
+    dataType: "json"
+  }).done(function (responseData) {
+    for (i = 0; i < responseData["candidates"].length; i++) {
+      var candidate = responseData["candidates"][i]["name"];      //You can add the variable before actually doing the loop
+      var votes = responseData["candidates"][i]["votes"];
+      $("#list").append("<li>" + candidate + "</li>" + "<li class = 'vote'>" + votes + "</li>"); // Inserting the variables with HTML
+    };
+  });
+
+$("#refresh").click(function() {
+  $.ajax({
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    data: {},
+    datatype: "json"
+  }).done (function (responseData) {
+    for (i = 0; i < responseData["candidates"].length; i++) {
+      var votes = responseData["candidates"][i]["votes"];
+      var voteIndex = (i + 1) * 2;
+      $(".vote:nth-child(" + voteIndex + ")").text(votes);  //Using a variable inside of the selector
+    };
+  });
+});
 
 });
